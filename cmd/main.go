785246b5fc4aa732918a3d8a8b3d8dbf66dbe4e3
Ubuntu <ubuntu@ip-172-31-27-236.us-west-2.compute.internal>
2021-04-27 10:16:38 +0000

    c70616

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,6 @@ func handler(w http.ResponseWriter, r *http.Request){
 	if err != nil {
 		host = "unknown host"
 	}
-
 	resp := fmt.Sprintf("Hello from %s", host)
 	_, err = w.Write([]byte(resp))
 	if err != nil {
