c338b2ba7e876695b55455b4988cfafc2fd549d7
Ubuntu <ubuntu@ip-172-31-27-236.us-west-2.compute.internal>
2021-04-28 09:36:10 +0000

    c81736

--- cmd/main.go
+++ cmd/main.go
@@ -15,6 +15,7 @@ func main() {
 
 func startServer(handler func(http.ResponseWriter, *http.Request)){
 	http.HandleFunc("/", handler)
+
 	log.Printf("starting server...")
 	http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil)
 }
